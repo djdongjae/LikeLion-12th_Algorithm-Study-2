e2a6f9555dc5794abe4cdef046f5b03a5a8516c0
djdongjae <jk47wee@gmail.com>
2025-02-18 22:55:01 +0900

    [BOJ] 1926 그림 / 실버 1 / 30분

---
+++ dongjae/ALGO/src/boj_1926/Main.java
@@ -0,0 +1,62 @@
+package boj_1926;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[][] map;
+    public static boolean[][] visited;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, -1, 0, 1};
+    public static int area;
+    public static int areaCount = 0;
+    public static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    area = 1;
+                    dfs(i, j);
+                    areaCount++;
+                    max = Math.max(max, area);
+                }
+            }
+        }
+
+        System.out.println(areaCount);
+        System.out.println(max);
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    area++;
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
